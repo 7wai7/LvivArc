3f90ee1dd75661f8b1792949f2ff1a9f50de0392
7wai7 <156088083+7wai7@users.noreply.github.com>
2024-09-18 11:14:25 +0300

    Delete views.js

--- views.js
+++
@@ -1,8 +0,0 @@
-exports.main = (req, res) => {
-    let Datastore = require('nedb')
-    let db = new Datastore({ filename: 'lvivarc.json', autoload: true })
-    db.find({ table:"arctype" }, (err, arctypes) => {
-    res.render(lpath + 'index', { types: arctypes })
-    })
-   } 
-   
